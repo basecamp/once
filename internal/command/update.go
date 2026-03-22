2ce24074d91200a1146fa9eb129560a1945c6923
Florent <florent.destremau@gmail.com>
2026-03-22 23:03:31 +0100

    Allow updating a specific app via once update <app>

--- internal/command/update.go
+++ internal/command/update.go
@@ -1,8 +1,12 @@
 package command
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/spf13/cobra"
 
+	"github.com/basecamp/once/internal/docker"
 	"github.com/basecamp/once/internal/version"
 )
 
@@ -13,12 +17,47 @@ type updateCommand struct {
 func newUpdateCommand() *updateCommand {
 	u := &updateCommand{}
 	u.cmd = &cobra.Command{
-		Use:   "update",
-		Short: "Update once to the latest version",
-		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return version.NewUpdater().UpdateBinary()
-		},
+		Use:   "update [app]",
+		Short: "Update once to the latest version, or update a specific application",
+		Args:  cobra.MaximumNArgs(1),
+		RunE:  WithNamespace(u.run),
 	}
 	return u
+}
+
+// Private
+
+func (u *updateCommand) run(ctx context.Context, ns *docker.Namespace, cmd *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return version.NewUpdater().UpdateBinary()
+	}
+
+	appName := args[0]
+	progress := func(p docker.DeployProgress) {
+		switch p.Stage {
+		case docker.DeployStageDownloading:
+			fmt.Printf("Downloading: %d%%\n", p.Percentage)
+		case docker.DeployStageStarting:
+			fmt.Println("Starting...")
+		case docker.DeployStageFinished:
+			fmt.Println("Finished")
+		}
+	}
+
+	var changed bool
+	err := withApplication(ns, appName, "updating", func(app *docker.Application) error {
+		var err error
+		changed, err = app.Update(ctx, progress)
+		return err
+	})
+	if err != nil {
+		return err
+	}
+
+	if changed {
+		fmt.Printf("Updated %s\n", appName)
+	} else {
+		fmt.Printf("%s is already up to date\n", appName)
+	}
+	return nil
 }
